026364f16aa88326cd94a516b80c28f65fd406c4
Himandri Sharma <himandrisharma27@gmail.com>
2022-11-25 20:41:45 +0530

    added roleList

--- src/App.tsx
+++ src/App.tsx
@@ -2,36 +2,13 @@ import React, { useState } from "react";
 import { ethers } from "ethers";
 import MemberRole from "./artifacts/contracts/MemberRole.sol/MemberRole.json";
 import "./App.css";
-
-const CONTRACT_ADDRESS = "0x8A791620dd6260079BF849Dc5567aDC3F2FdC318";
+import RoleList from "./pages/RoleList";
 
 function App() {
-	const [roleTypes, setRoleTypes] = useState(0);
-	const fetchRoleTypes = async () => {
-		// const accounts = await window.ethereum.request({
-		// 	method: "eth_requestAccounts",
-		// });
-		if (typeof window.ethereum !== undefined) {
-			const provider = new ethers.providers.Web3Provider(window.ethereum);
-      await provider.send('eth_requestAccounts', []); 
-			const signer = provider.getSigner();
-			const contract = new ethers.Contract(
-				CONTRACT_ADDRESS,
-				MemberRole.abi,
-				signer
-			);
-			try {
-				const data = await contract.addresses("0");
-				// await data.wait();
-        console.log(data)
-			} catch (error) {
-				console.log("Error", error);
-			}
-		}
-	};
 	return (
 		<div className="App">
-			hello dapp<button onClick={fetchRoleTypes}>click</button>
+			hello dapp
+			<RoleList />
 		</div>
 	);
 }
--- src/pages/RoleList.tsx
+++ src/pages/RoleList.tsx
@@ -1,9 +1,59 @@
-import React from 'react'
+import { useEffect, useState } from "react";
+import { ethers } from "ethers";
+import MemberRole from "../artifacts/contracts/MemberRole.sol/MemberRole.json";
+import { CONTRACT_ADDRESS } from "../config";
+import { Button, Card } from "antd";
 
 function RoleList() {
-  return (
-    <div>RoleList</div>
-  )
+	const [roleTypes, setRoleTypes] = useState(0);
+	const [roleNames, setRoleNames] = useState<any>([]);
+	useEffect(() => {
+		fetchRoleTypes();
+	}, []);
+	const fetchRoleTypes = async () => {
+		if (typeof window.ethereum !== undefined) {
+			const provider = new ethers.providers.Web3Provider(window.ethereum);
+			await provider.send("eth_requestAccounts", []);
+			const signer = provider.getSigner();
+			const contract = new ethers.Contract(
+				CONTRACT_ADDRESS,
+				MemberRole.abi,
+				signer
+			);
+			try {
+				const roles = await contract.roleTypesCount();
+				setRoleTypes(roles.toNumber());
+				for (let i = 0; i < roles.toNumber(); i++) {
+					let role = await contract.roleTypes(i);
+					setRoleNames([...roleNames, role]);
+				}
+			} catch (error) {
+				console.log("Error", error);
+			}
+		}
+	};
+	return (
+		<span
+			style={{
+				height: "100vh",
+				width: "100vw",
+				display: "flex",
+				flexDirection: "column",
+				justifyContent: "center",
+				alignItems: "center",
+			}}
+		>
+			{roleTypes !== 0 ? (
+				roleNames.map((val: any, i: any) => (
+					<Card key={i} style={{ width: 300, margin: "10px" }}>
+						{val}
+					</Card>
+				))
+			) : (
+				<Card style={{ width: 300 }}>No Role Types declared yet.</Card>
+			)}
+		</span>
+	);
 }
 
-export default RoleList
+export default RoleList;

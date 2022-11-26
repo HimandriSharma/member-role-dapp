6288a2d5784642a6c8a68fe4591993e75b0d85a7
Himandri Sharma <himandrisharma27@gmail.com>
2022-11-26 14:08:02 +0530

    tab spacing

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,17 +1,22 @@
-import { Card } from 'antd'
-import React from 'react'
-import { Link } from 'react-router-dom'
-import "../App.css"
+import { Card } from "antd";
+import React from "react";
+import { Link } from "react-router-dom";
+import "../App.css";
 
 function Home() {
-  return (
-    <span className='center-page'>
-        <Card style={{width:500}}>
-            <span>Create a <Link to="/list_roles">Role Type</Link></span><br/>
-            <span>Create or Update <Link to="/create_update_role">Member</Link></span>
-        </Card>
-    </span>
-  )
+	return (
+		<span className="center-page">
+			<Card style={{ width: 500 }}>
+				<span>
+					Create a <Link to="/list_roles">Role Type</Link>
+				</span>
+				<br />
+				<span>
+					Create or Update <Link to="/create_update_role">Member</Link>
+				</span>
+			</Card>
+		</span>
+	);
 }
 
-export default Home
+export default Home;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,7 +7,7 @@ import Warning from "./Warning";
 function index() {
 	return (
 		<Routes>
-      <Route path="*" element={<Warning/>}/>
+			<Route path="*" element={<Warning />} />
 			<Route path="/" element={<Home />} />
 			<Route path="/create_update_role" element={<NewRole />} />
 			<Route path="/list_roles" element={<RoleList />} />
